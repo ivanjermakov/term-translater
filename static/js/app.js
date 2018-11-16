357c0a1fa17cd6a70d6ef4a71625bc68f3bbcce0
ivanjermakov <vania6600@gmail.com>
2018-11-16 12:27:40 +0300

    complete frontend

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,8 @@ let app = angular.module("app", []);
 
 app.controller('MainController', ($document, $scope, $http) => {
 
+	$scope.SEARCH_RESULTS_AMOUNT = 5;
+
 	$scope.languages = [];
 
 	$scope.from = null;
@@ -9,6 +11,8 @@ app.controller('MainController', ($document, $scope, $http) => {
 
 	$scope.search = "";
 	$scope.result = "";
+
+	$scope.suggestions = [];
 
 	$scope.initLanguages = () => {
 		$http({
@@ -24,7 +28,38 @@ app.controller('MainController', ($document, $scope, $http) => {
 	})();
 
 	$scope.updateSearch = () => {
-		$scope.result = $scope.search
+		let currentSearch = $scope.search;
+
+		setTimeout(() => {
+			if (currentSearch === $scope.search) {
+				$http({
+					method: 'GET',
+					url: '/s',
+					params: {
+						"l": $scope.from,
+						"s": $scope.search
+					}
+				}).then((response) => {
+						$scope.suggestions = response.data;
+						$scope.suggestions = $scope.suggestions.slice(0, $scope.SEARCH_RESULTS_AMOUNT)
+					}
+				)
+			}
+		}, 200);
+	};
+
+	$scope.translate = (suggestion) => {
+		$http({
+			method: 'GET',
+			url: '/t',
+			params: {
+				"from": $scope.from,
+				"to": $scope.to,
+				"title": suggestion
+			}
+		}).then((response) => {
+			$scope.result = response.data;
+		})
 	};
 
 });
